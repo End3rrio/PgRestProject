cfb3d9829eb1066fae92c33484f7f08a8b429af9
Kirill <mikhienkov@inbox.ru>
2023-07-17 23:09:12 +0300

    Strart

---
+++ src/main/java/com/example/pgrestproject/PgRestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pgrestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgRestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgRestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pgrestproject/PgRestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pgrestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgRestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
